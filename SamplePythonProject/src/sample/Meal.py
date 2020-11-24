0fe0dcf7705ec4afac93ea68a9dc5b04ecce5b43
jszczepanski <jackobgfx@gmail.com>
2020-11-24 23:55:01 +0100

    more_tests

--- SamplePythonProject/src/sample/Meal.py
+++ SamplePythonProject/src/sample/Meal.py
@@ -14,3 +14,22 @@ class Meal:
 
         meal = requests.get(f'https://www.themealdb.com/api/json/v1/1/search.php?s={name_value}').json()
         return meal["meals"][0]["strMeal"]
+
+    def random_meal(self):
+        return requests.get('https://www.themealdb.com/api/json/v1/1/random.php').json()
+
+    def meal_letter(self, letter):
+        if type(letter) != str:
+            raise TypeError("Podaj właściwy format")
+
+        meal = requests.get(f'https://www.themealdb.com/api/json/v1/1/search.php?f={letter}').json()
+        return meal["meals"][0]
+
+    def meal_ingr(self, ingr):
+        if type(ingr) != str:
+            raise TypeError("Podaj właściwy format")
+
+        meal = requests.get(f'https://www.themealdb.com/api/json/v1/1/filter.php?i={ingr}').json()
+        return meal["meals"][0]
+
+
--- SamplePythonProject/tests/MealTest.py
+++ SamplePythonProject/tests/MealTest.py
@@ -1,4 +1,5 @@
 import unittest
+import requests
 
 from src.sample.Meal import *
 
@@ -12,6 +13,27 @@ class MealTest(unittest.TestCase):
 
     def test_meal__name(self):
         self.assertEqual(self.tmp.meal__name("Arrabiata"), "Spicy Arrabiata Penne")
+
+    def test_meal__letter_id(self):
+        self.assertEqual(self.tmp.meal_letter("b")["idMeal"], "52767")
+
+    def test_meal__letter_category(self):
+        self.assertEqual(self.tmp.meal_letter("d")["strCategory"], "Vegetarian")
+
+    def test_meal__letter_area(self):
+        self.assertEqual(self.tmp.meal_letter("e")["strArea"], "British")
+
+    def test_meal__letter_str(self):
+        self.assertEqual(self.tmp.meal_letter("c")["strMeal"], "Chocolate Gateau")
+
+    def test_meal_ingr(self):
+        self.assertEqual(self.tmp.meal_ingr("chicken_breast")["strMeal"], "Chick-Fil-A Sandwich")
+
+
+    ["strMeal"]
+
+    #def test_random_meal(self):
+    #    self.assertEqual(self.tmp.random_meal(), requests.get('https://www.themealdb.com/api/json/v1/1/random.php').json())
 
 
 
