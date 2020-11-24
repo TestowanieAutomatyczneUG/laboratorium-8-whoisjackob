477523a0690142a2336b08b4a7beee0f3adf1f92
jszczepanski <jackobgfx@gmail.com>
2020-11-24 23:22:56 +0100

    meal_name

--- SamplePythonProject/src/sample/Meal.py
+++ SamplePythonProject/src/sample/Meal.py
@@ -7,3 +7,10 @@ class Meal:
 
         meal = requests.get(f'https://www.themealdb.com/api/json/v1/1/lookup.php?i={id_value}').json()
         return meal["meals"][0]["strMeal"]
+
+    def meal__name(self, name_value):
+        if type(name_value) != str:
+            raise TypeError("Podaj właściwy format")
+
+        meal = requests.get(f'https://www.themealdb.com/api/json/v1/1/search.php?s={name_value}').json()
+        return meal["meals"][0]["strMeal"]
--- SamplePythonProject/tests/MealTest.py
+++ SamplePythonProject/tests/MealTest.py
@@ -10,4 +10,9 @@ class MealTest(unittest.TestCase):
     def test_meal__id(self):
         self.assertEqual(self.tmp.meal__id(52772), "Teriyaki Chicken Casserole")
 
+    def test_meal__name(self):
+        self.assertEqual(self.tmp.meal__name("Arrabiata"), "Spicy Arrabiata Penne")
+
+
+
 
